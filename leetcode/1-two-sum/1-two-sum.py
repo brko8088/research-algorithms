9fcacb544d3816b157673d2b63f263eff2a36cc1
Bruno <brunokoppel.dev@gmail.com>
2025-01-26 22:33:55 -0500

    progress

--- leetcode/1-two-sum/1-two-sum.py
+++ leetcode/1-two-sum/1-two-sum.py
@@ -6,12 +6,9 @@ class Solution(object):
     :rtype: List[int]
     """
 
-    arrayLength = len(nums)
-    rtype = []
-    
-    for i in range(0, arrayLength):
-      for j in range (i + 1, arrayLength):
+    for i in range(0, len(nums)):
+      for j in range (i + 1, len(nums)):
         if (nums[i] + nums[j] == target):
-          rtype.append(i)
-          rtype.append(j)
-          return rtype
+          return [i,j]
+        
+    return []
