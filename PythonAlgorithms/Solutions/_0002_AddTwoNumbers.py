e2aa2d23bb0af9f3ec2450cb12f5b743fef6e1b7
Bruno <brunokoppel.dev@gmail.com>
2025-02-19 21:22:23 -0500

    Full python scripts

--- PythonAlgorithms/Solutions/_0002_AddTwoNumbers.py
+++ PythonAlgorithms/Solutions/_0002_AddTwoNumbers.py
@@ -2,7 +2,6 @@ import sys
 from typing import Optional
 
 sys.path.append('../')
-
 from Classes.ListNode import ListNode
 
 # Definition for singly-linked list.
@@ -13,66 +12,32 @@ from Classes.ListNode import ListNode
 
 class _0002_AddTwoNumbers:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        cl1 = l1
-        cl2 = l2
-        xl1 = []
-        xl2 = []
+        result = None
+        current = result
+        reminder = 0
         
-        while (cl1 is not None or cl2 is not None):
-            if (cl1 is not None):
-                xl1.append(cl1.val)
-                if (cl1.next is None):
-                    cl1.val = 0
-                cl1 = cl1.next
+        while (l1 is not None or l2 is not None):
+            if result is None:
+                result = ListNode()
+                current = result
+            
+            if (l1 is not None):
+                current.val += l1.val
+                l1 = l1.next
                 
-            if (cl2 is not None):
-                xl2.append(cl2.val)
-                if (cl2.next is None):
-                    cl2.val = 0
-                cl2 = cl2.next
-
-        highest_number = 0 
-        if (len(xl1) > len(xl2)):
-            highest_number = len(xl1)
-        else:
-            highest_number = len(xl2)
-
-        res_node = None
-        cur_node = None
-        carry_over = 0
+            if (l2 is not None):
+                current.val += l2.val
+                l2 = l2.next
+                
+            if (current.val >= 10):
+                reminder = 1
+                current.val -= 10
+                
+            if (l1 is not None or l2 is not None or reminder > 0):
+                current.next = ListNode(reminder)
+                current = current.next
+            
+            reminder = 0
+            
+        return result
         
-        for x in range(0, highest_number):
-            if (res_node is None):
-                res_node = ListNode()
-                carry_over = self.remCal(xl1, xl2, x, carry_over)
-                res_node.val = ((self.calculation(xl1, xl2, x)) % 10)
-                if x < (highest_number - 1) or carry_over > 0:
-                    res_node.next = ListNode()
-                    cur_node = res_node.next
-                    cur_node.val = carry_over
-                    
-            else:
-                carry_over = self.remCal(xl1, xl2, x, carry_over)
-                cur_node.val = (cur_node.val + self.calculation(xl1, xl2, x))  % 10
-                if x < (highest_number - 1) or carry_over > 0:
-                    cur_node.next = ListNode()
-                    cur_node = cur_node.next
-                    cur_node.val = carry_over
-        
-        return res_node
-        
-    def calculation(self, a1, a2, x):
-        if x < len(a1) and x < len(a2):
-            return a1[x] + a2[x]
-        elif x < len(a1):
-            return a1[x]
-        elif x < len(a2):
-            return a2[x]
-        else:
-            return -1
-                                   
-    def remCal(self, a1, a2, x, carry_over):
-        if (self.calculation(a1, a2, x) + carry_over) > 9:
-            return 1
-        return 0
-    
--- PythonAlgorithms/UnitTests/_0002_AddTwoNumbers_Test.py
+++ PythonAlgorithms/UnitTests/_0002_AddTwoNumbers_Test.py
@@ -6,14 +6,13 @@ from Classes.ListNode import ListNode
 from Solutions._0002_AddTwoNumbers import _0002_AddTwoNumbers
 
 class _0002_AddTwoNumbers_Test(unittest.TestCase):
-
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    def _array_to_list(self, arr):
+    def _arrayToListNode(self, arr):
         if not arr:
             return None
         head = ListNode(arr[0])
@@ -23,32 +22,104 @@ class _0002_AddTwoNumbers_Test(unittest.TestCase):
             node = node.next
         return head
 
-    def _list_to_array(self, head):
+    def _listNodeToArray(self, head):
         if not head:
             return []
         arr = []
         node = head
-        while node:
+        while node is not None and node.val is not None:
             arr.append(node.val)
             node = node.next
         return arr
 
     def test_AddTwoNumbersTest_Empty(self):
+        listNode = self._arrayToListNode([])
         solution = _0002_AddTwoNumbers()
-        head = s.addTwoNumbers(None, None)
-        self.assertIsNone(head)
+        result = solution.addTwoNumbers(listNode, listNode)
+        self.assertEqual(self._listNodeToArray(result), [])
 
-#   def test_add_two_numbers(self):
-#       s = _0002_AddTwoNumbers()
-#       for i, o in [(([], []), []),
-#                 (([0], [1]), [1]),
-#                 (([1, 8], [0]), [1, 8]),
-#                 (([2, 4, 3], [5, 6, 4]), [7, 0, 8]),
-#                 (([0, 1], [9, 9, 9, 9]), [9, 0, 0, 0, 1])]:
-#         head = s.addTwoNumbers(self._array_to_list(
-#             i[0]), self._array_to_list(i[1]))
-#         arr = self._list_to_array(head)
-#         self.assertEqual(arr, o)
+    def test_AddTwoNumbersTest_General(self):
+        listNode = self._arrayToListNode([2, 4, 3])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode, listNode)
+        self.assertEqual(self._listNodeToArray(result), [4, 8, 6])
+        
+    def test_AddTwoNumbersTest_General2(self):
+        listNode1 = self._arrayToListNode([2, 4, 3])
+        listNode2 = self._arrayToListNode([5, 6, 4])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [7, 0, 8])
+        
+    def test_AddTwoNumbersTest_HasCarry(self):
+        listNode1 = self._arrayToListNode([9, 9, 9])
+        listNode2 = self._arrayToListNode([9, 9, 9])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [8, 9, 9, 1])
+        
+    def test_AddTwoNumbersTest_HasCarry2(self):
+        listNode1 = self._arrayToListNode([2, 4, 3])
+        listNode2 = self._arrayToListNode([5, 6, 4])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [7, 0, 8 ]) 
+    
+    def test_AddTwoNumbersTest_HasMultipleCarry(self):
+        listNode1 = self._arrayToListNode([ 1 ])
+        listNode2 = self._arrayToListNode([ 9, 9])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [0, 0, 1])
+        
+    def test_AddTwoNumbersTest_FirstNumCarry(self):
+        listNode1 = self._arrayToListNode([ 3, 4, 4 ])
+        listNode2 = self._arrayToListNode([ 3, 4, 6 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [6, 8, 0, 1])
+        
+    def test_AddTwoNumbersTest_L1Longer(self):
+        listNode1 = self._arrayToListNode([ 2, 4, 3, 1 ])
+        listNode2 = self._arrayToListNode([ 5, 6, 4 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 7, 0, 8, 1 ])
+        
+    def test_AddTwoNumbersTest_L1Longer_2(self):
+        listNode1 = self._arrayToListNode([ 1, 8 ])
+        listNode2 = self._arrayToListNode([ 0 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 1, 8 ])
+        
+    def test_AddTwoNumbersTest_L1Empty(self):
+        listNode1 = self._arrayToListNode(None)
+        listNode2 = self._arrayToListNode([ 1, 8 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 1, 8 ])
+    
+    def test_AddTwoNumbersTest_L2Longer(self):
+        listNode1 = self._arrayToListNode([ 2, 4, 3 ])
+        listNode2 = self._arrayToListNode([ 5, 6, 4, 3 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 7, 0, 8, 3 ])
+    
+    def test_AddTwoNumbersTest_L2Longer_2(self):
+        listNode1 = self._arrayToListNode([ 0 ])
+        listNode2 = self._arrayToListNode([ 1, 8 ])
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 1, 8 ])
+
+    def test_AddTwoNumbersTest_L2Empty(self):
+        listNode1 = self._arrayToListNode([ 1, 8 ])
+        listNode2 = self._arrayToListNode(None)
+        solution = _0002_AddTwoNumbers()
+        result = solution.addTwoNumbers(listNode1, listNode2)
+        self.assertEqual(self._listNodeToArray(result), [ 1, 8 ])
 
 if __name__ == '__main__':
     unittest.main()
